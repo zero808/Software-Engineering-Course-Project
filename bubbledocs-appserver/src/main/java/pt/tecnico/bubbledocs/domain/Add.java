57ab6ed067dad935a82bd18d3d3b34421756e5da
Marco Cunha <marco.soudocunha@gmail.com>
2015-03-07 12:58:26 +0000

    Alteracao (nao funciona) continuar o refactor

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Add.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Add.java
@@ -8,14 +8,13 @@ public class Add extends Add_Base {
 
 	public Add(Content arg1, Content arg2) {
 		super();
-		super.setContent1(arg1);
-		super.setContent2(arg2);
+		super.setArg1(arg1);
+		super.setArg2(arg2);
 	}
 
 	@Override
 	public int getValue() {
-
-		return super.getContent1().getValue() + super.getContent2().getValue();
+		return super.getArg1().getValue() + super.getArg2().getValue();
 		// TODO Throw exception
 	}
 
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Binary.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Binary.java
@@ -2,13 +2,40 @@ package pt.tecnico.bubbledocs.domain;
 
 public abstract class Binary extends Binary_Base {
 
+	private Content arg1;
+	private Content arg2;
+
 	public Binary() {
 		super();
 	}
 
 	public Binary(Content arg1, Content arg2) {
 		super();
-		super.setContent1(arg1);
-		super.setContent2(arg2);
+		this.arg1 = arg1;
+		this.arg2 = arg2;
+	}
+
+	public Content getArg1() {
+		return arg1;
+	}
+
+	public void setArg1(Content arg1) {
+		this.arg1 = arg1;
+	}
+
+	public Content getArg2() {
+		return arg2;
+	}
+
+	public void setArg2(Content arg2) {
+		this.arg2 = arg2;
+	}
+
+	@Override
+	public void delete() {
+		this.arg1 = null;
+		this.arg2 = null;
+		setCell(null);
+		deleteDomainObject();
 	}
 }
