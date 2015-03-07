b56bae6f885ce6c483a2d6d1dfb446a23b3bb8ed
loading27 <loadingtosh27@gmail.com>
2015-03-07 10:34:36 +0000

    Commit Add

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Add.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Add.java
@@ -1,9 +1,22 @@
 package pt.tecnico.bubbledocs.domain;
 
 public class Add extends Add_Base {
-    
-    public Add() {
-        super();
-    }
-    
+
+	public Add() {
+		super();
+	}
+
+	public Add(Content arg1, Content arg2) {
+		super();
+		super.setContent1(arg1);
+		super.setContent2(arg2);
+	}
+
+	@Override
+	public int getValue() {
+
+		return super.getContent1().getValue() + super.getContent2().getValue();
+		// TODO Throw exception
+	}
+
 }
