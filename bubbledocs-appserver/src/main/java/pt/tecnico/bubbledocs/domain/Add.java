e17783fd5cb7c36ba6c6e4ce3747a10f6c023357
Marco Cunha <marco.soudocunha@gmail.com>
2015-03-10 10:57:38 +0000

    Alteracoes estrutura argumentos

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Add.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Add.java
@@ -6,15 +6,41 @@ public class Add extends Add_Base {
 		super();
 	}
 
-	public Add(Content arg1, Content arg2) {
+	public Add(Literal arg1, Literal arg2) {
 		super();
-		super.setArg1(arg1);
-		super.setArg2(arg2);
+		super.addLiterals(arg1);
+		super.addLiterals(arg2);
+	}
+
+	public Add(Reference arg1, Literal arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addLiterals(arg2);
+	}
+
+	public Add(Literal arg1, Reference arg2) {
+		super();
+		super.addReferences(arg2);
+		super.addLiterals(arg1);
+	}
+
+	public Add(Reference arg1, Reference arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addReferences(arg2);
 	}
 
 	@Override
 	public int getValue() {
-		return super.getArg1().getValue() + super.getArg2().getValue();
+		int value = 0;
+		if (super.getReferencesSet().size() + super.getLiteralsSet().size() != 2) {
+			// TODO throw exception, number of args invalid
+		}
+		for (Reference r : super.getReferencesSet())
+			value += r.getValue();
+		for (Literal l : super.getLiteralsSet())
+			value += l.getValue();
+		return value;
 		// TODO Throw exception
 	}
 
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Mul.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Mul.java
@@ -1,21 +1,48 @@
 package pt.tecnico.bubbledocs.domain;
 
+import java.util.ArrayList;
+
 public class Mul extends Mul_Base {
 
 	public Mul() {
 		super();
 	}
 
-	public Mul(Content arg1, Content arg2) {
+	public Mul(Literal arg1, Literal arg2) {
 		super();
-		super.setArg1(arg1);
-		super.setArg2(arg2);
+		super.addLiterals(arg1);
+		super.addLiterals(arg2);
+	}
+
+	public Mul(Reference arg1, Literal arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addLiterals(arg2);
+	}
+
+	public Mul(Literal arg1, Reference arg2) {
+		super();
+		super.addReferences(arg2);
+		super.addLiterals(arg1);
+	}
+
+	public Mul(Reference arg1, Reference arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addReferences(arg2);
 	}
 
 	@Override
 	public int getValue() {
-
-		return super.getArg1().getValue() * super.getArg2().getValue();
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		if (super.getReferencesSet().size() + super.getLiteralsSet().size() != 2) {
+			// TODO throw exception, number of args invalid
+		}
+		for (Reference r : super.getReferencesSet())
+			values.add(r.getValue());
+		for (Literal l : super.getLiteralsSet())
+			values.add(l.getValue());
+		return values.get(0) * values.get(1);
 		// TODO Throw exception
 	}
 
