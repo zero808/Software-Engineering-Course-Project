d35081873ab76f144bd914468bf92dc8b2e158e5
Marco Cunha <marco.soudocunha@gmail.com>
2015-03-10 10:57:38 +0000

    Alteracoes estrutura argumentos

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Binary.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Binary.java
@@ -2,39 +2,39 @@ package pt.tecnico.bubbledocs.domain;
 
 public abstract class Binary extends Binary_Base {
 
-	private Content arg1;
-	private Content arg2;
-
 	public Binary() {
 		super();
 	}
 
-	public Binary(Content arg1, Content arg2) {
+	public Binary(Literal arg1, Literal arg2) {
 		super();
-		this.arg1 = arg1;
-		this.arg2 = arg2;
+		super.addLiterals(arg1);
+		super.addLiterals(arg2);
 	}
 
-	public Content getArg1() {
-		return arg1;
+	public Binary(Reference arg1, Literal arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addLiterals(arg2);
 	}
 
-	public void setArg1(Content arg1) {
-		this.arg1 = arg1;
+	public Binary(Literal arg1, Reference arg2) {
+		super();
+		super.addReferences(arg2);
+		super.addLiterals(arg1);
 	}
 
-	public Content getArg2() {
-		return arg2;
-	}
-
-	public void setArg2(Content arg2) {
-		this.arg2 = arg2;
+	public Binary(Reference arg1, Reference arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addReferences(arg2);
 	}
 
 	public void delete() {
-		this.arg1 = null;
-		this.arg2 = null;
-		setCell(null);
-		deleteDomainObject();
+		// this.arg1 = null;
+		// this.arg2 = null;
+		// setCell(null);
+		// deleteDomainObject();
+		// TODO
 	}
 }
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Div.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Div.java
@@ -1,21 +1,48 @@
 package pt.tecnico.bubbledocs.domain;
 
+import java.util.ArrayList;
+
 public class Div extends Div_Base {
 
 	public Div() {
 		super();
 	}
 
-	public Div(Content arg1, Content arg2) {
+	public Div(Literal arg1, Literal arg2) {
 		super();
-		super.setArg1(arg1);
-		super.setArg2(arg2);
+		super.addLiterals(arg1);
+		super.addLiterals(arg2);
+	}
+
+	public Div(Reference arg1, Literal arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addLiterals(arg2);
+	}
+
+	public Div(Literal arg1, Reference arg2) {
+		super();
+		super.addReferences(arg2);
+		super.addLiterals(arg1);
+	}
+
+	public Div(Reference arg1, Reference arg2) {
+		super();
+		super.addReferences(arg1);
+		super.addReferences(arg2);
 	}
 
 	@Override
 	public int getValue() {
-
-		return super.getArg1().getValue() / super.getArg2().getValue();
+		ArrayList<Integer> values = new ArrayList<Integer>();
+		if (super.getReferencesSet().size() + super.getLiteralsSet().size() != 2) {
+			// TODO throw exception, number of args invalid
+		}
+		for (Reference r : super.getReferencesSet())
+			values.add(r.getValue());
+		for (Literal l : super.getLiteralsSet())
+			values.add(l.getValue());
+		return values.get(0) / values.get(1);
 		// TODO Throw exception
 	}
 
