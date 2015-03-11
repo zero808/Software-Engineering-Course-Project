f8aae6146330d476b0c6922385072e88a4b47f72
Luis Manuel Ribeiro Farinha <luis.farinha@tecnico.ulisboa.pt>
2015-03-11 11:01:40 +0000

    Second go at permissions. Corrigi as issues que estavam no github em relacao as permissions, mais detalhes ai. Listar utilizadores agora nao lista o root. Criado o metodo que dado um intervalo, devolve todas as celulas dele. Varios bug fixes e outras coisas, principalmente em relacao a remocao de users e spreadsheets e permissoes em geral As funcoes para adicionar cenas nas spreadsheets agora verificam se as celulas estao protegidas ou nao. Criadas as excepcoes para suporte. Possivelmente mais coisas que agora nao me lembro, provavelmente nao serao importantes. Listar os utilizadores agora mostra tambem as suas passwords como especificado no enunciado.

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Permission.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Permission.java
@@ -4,28 +4,31 @@ import pt.tecnico.bubbledocs.exception.SpreadsheetDoesNotExistException;
 import pt.tecnico.bubbledocs.exception.UserDoesNotExistException;
 
 public class Permission extends Permission_Base {
-    
-    public Permission(Spreadsheet spred, User user, boolean b) throws SpreadsheetDoesNotExistException, UserDoesNotExistException{
-    	
-        if(spred == null) throw new SpreadsheetDoesNotExistException("NULL");
-        if(user == null) throw new UserDoesNotExistException("NULL");
-        
-        setRw(b);
-        setUser(user);
-        setSpreadsheet(spred);
-        
-        spred.addPermissions(this);
-        user.addSpreadsheets(spred);
-        
-        
-    }
-    
-    public void edit(boolean b){
-    	setRw(b);
-    }
-    
-    public void delete(){
-    	deleteDomainObject();
-    }
-    
-}
+
+	public Permission(Spreadsheet spred, User user, boolean b) throws SpreadsheetDoesNotExistException, UserDoesNotExistException {
+
+		if (spred == null)
+			throw new SpreadsheetDoesNotExistException("NULL");
+		if (user == null)
+			throw new UserDoesNotExistException("NULL");
+
+		setRw(b);
+		setUser(user);
+		setSpreadsheet(spred);
+
+		spred.addPermissions(this);
+		user.addSpreadsheets(spred);
+
+	}
+
+	public void edit(boolean b) {
+		setRw(b);
+	}
+
+	public void delete() {
+		setUser(null);
+		setSpreadsheet(null);
+		deleteDomainObject();
+	}
+
+}// End of Permission Class
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Spreadsheet.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Spreadsheet.java
@@ -1,40 +1,126 @@
 package pt.tecnico.bubbledocs.domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.joda.time.DateTime;
 
+import pt.tecnico.bubbledocs.exception.OutofBondsException;
+
 public class Spreadsheet extends Spreadsheet_Base {
-	
-    public Spreadsheet(String name, DateTime date, int nRows, int nCollumns) {
-        super();
-        
-        int _idnext;
-		
+
+	public Spreadsheet(String name, DateTime date, int nRows, int nCollumns) {
+		super();
+
+		int _idnext;
+
 		BubbleDocs bd = getBubbledocs();
 		_idnext = bd.getIdGlobal();
 		_idnext++;
-		
-		setId(_idnext);
-	    setName(name);
-	    setDate(date);
-	    setNRows(nRows);
-	    setNCols(nCollumns);
-    }
-    
-    public void delete() {
-    	
-		setUser(null);
-		deleteDomainObject();
-    }
 
-    public void printCells() {
-    	 
-		for (Cell cell : getCellsSet()) {
-	    	cell.toString(); //toString precisa de ser feito para cada tipo de conteudo
+		setId(_idnext);
+		setName(name);
+		setDate(date);
+		setNRows(nRows);
+		setNCols(nCollumns);
+	}
+
+	public void delete() { //Used to remove connection to user that is being removed.
+
+		setUser(null);
+	}
+	
+	public void deleteSpreadsheetContent() { //Used to remove a spreadsheet.
+		
+		setBubbledocs(null);
+		setUser(null);
+		for(Permission p : getPermissionsSet()) {
+			if(p.getSpreadsheet().getName().equals(getName())) {
+				p.delete();
 			}
-    }
-    
-    public String toString() {
-    	return "Nome:" + getName() + "Data Criacao:" + getDate() + "N-Linhas:" + getNRows() + "N-Colunas:" + getNCols();
-    }
-    
-}//End Spreadsheet Class
+		}
+		//for(Cell c : getCellsSet()) {
+			//c.delete(); Cell needs to implement this all the way down to remove spreadsheet.
+		//}
+		deleteDomainObject();
+		//TODO (Leo, Marco, Duarte)
+	}
+	
+	public Cell getCellByCoords(int row, int collumn) throws OutofBondsException {
+		Cell _requestedCell = null;
+		
+		for(Cell c : getCellsSet()) {
+			if(c.getRow() == row && c.getCollumn() == collumn) {
+				_requestedCell = c;
+			}
+		}
+		
+		if(_requestedCell == null) { //Should never happen, just to be safe.
+			throw new OutofBondsException(getName());
+		}
+		return _requestedCell;
+	}
+	
+	public List<Cell> getCellsInRange(Range r) {
+		List<Cell> _requestedCells = new ArrayList<Cell>();
+		Cell _startingCell = null;
+		Cell _endingCell = null;
+		int _rowIterator = 1;
+		int _collumnIterator = 1;
+		
+		for(Cell c : r.getCellsSet()) { //Should only have 2 cells in the set.
+			if(_startingCell == null) {
+				_startingCell = c; //Get the first cell.
+			}
+			if(_endingCell == null) {
+				_endingCell = c; //Get the last cell.
+			}
+		}
+		
+		_rowIterator = _startingCell.getRow(); //Get starting row value.
+		_collumnIterator = _startingCell.getCollumn(); //Get starting column value.
+		
+		if(_startingCell.getRow() == _endingCell.getRow()) {
+			while(_collumnIterator <= _endingCell.getCollumn()) {
+				_requestedCells.add(getCellByCoords(_startingCell.getRow(), _collumnIterator));
+				_collumnIterator++;
+			}
+		} else if(_startingCell.getCollumn() == _endingCell.getCollumn()) {
+			while(_rowIterator <= _endingCell.getRow()) {
+				_requestedCells.add(getCellByCoords(_rowIterator, _startingCell.getCollumn()));
+				_rowIterator++;
+			}
+		} else {
+			while(_rowIterator <= _endingCell.getRow()) {
+				while(_collumnIterator <= _endingCell.getCollumn()) {
+					_requestedCells.add(getCellByCoords(_rowIterator, _collumnIterator));
+					_collumnIterator++;
+				}
+				_collumnIterator = _startingCell.getCollumn(); //Reset of column iterator.
+				_rowIterator++;
+			}
+		}
+		return _requestedCells;
+	}
+	
+	public Permission getPermissionOfUser(User u) {
+		for(Permission p : getPermissionsSet()) {
+			if(p.getUser().getName().equals(u.getName())) {
+				return p;
+			}
+		}
+		return null;
+	}
+	
+	public void printCells() {
+
+		for (Cell cell : getCellsSet()) {
+			cell.toString();
+		}
+	}
+
+	public String toString() {
+		return "Nome:" + getName() + "Data Criacao:" + getDate() + "N-Linhas:" + getNRows() + "N-Colunas:" + getNCols();
+	}
+
+}// End Spreadsheet Class
