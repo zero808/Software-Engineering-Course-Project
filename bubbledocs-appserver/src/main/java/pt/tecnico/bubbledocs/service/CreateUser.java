90fd537cd8d252126ccd0a14a52bb787b9dfa664
Duarte Barreira <duartemtbarreira@gmail.com>
2015-03-26 19:35:44 +0000

    Criei o serviço para criar um utilizador, mas duas verificações estão comentadas porque precisam de ser implementadas

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateUser.java
@@ -1,16 +1,52 @@
 package pt.tecnico.bubbledocs.service;
 
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
+import pt.tecnico.bubbledocs.domain.User;
+import pt.tecnico.bubbledocs.exception.InvalidPermissionException;
+import pt.tecnico.bubbledocs.exception.InvalidUsernameException;
+import pt.tecnico.bubbledocs.exception.UserAlreadyExistsException;
+import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
 
 public class CreateUser extends BubbleDocsService {
+	
+	String userToken;
+	String newUsername;
+	String password;
+	String name;
+	BubbleDocs bd;
+	User user;
 
 	public CreateUser(String userToken, String newUsername,
 			String password, String name) {
-		// add code here
+		this.userToken = userToken;
+		this.newUsername = newUsername;
+		this.password = password;
+		this.name = name;
+		this.bd = BubbleDocs.getInstance();
 	}
 
 	@Override
 	protected void dispatch() throws BubbleDocsException {
-		// add code here
+		
+		//the user is not logged in
+		//if(!validToken(this.userToken))
+		//	throw new UserNotInSessionException(this.newUsername);
+		
+		//the username is already being used
+		if(this.bd.getUserByUsername(this.newUsername) != null)
+			throw new UserAlreadyExistsException(this.newUsername);
+		
+		//FIXME add the isRoot method
+		//only root can create new users
+//		if(!isRoot(this.userToken))
+//			throw new InvalidPermissionException(this.newUsername);
+		
+		//make sure the username is not the empty string
+		if(this.newUsername.equals(""))
+			throw new InvalidUsernameException(this.newUsername);
+		
+		user = new User(this.newUsername, this.name, this.password);
+		this.bd.addUsers(user);
 	}
 }
