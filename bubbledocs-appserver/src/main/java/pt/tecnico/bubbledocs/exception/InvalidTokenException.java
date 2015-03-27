bea96ab49e338971e81c6b0cd29ee377b26d2796
cmgalante <claudio.galante@tecnico.ulisboa.pt>
2015-03-27 07:33:25 +0000

    Inicio do serviço LoginUser

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/InvalidTokenException.java
@@ -0,0 +1,17 @@
+package pt.tecnico.bubbledocs.exception;
+
+import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+
+public class InvalidTokenException extends BubbleDocsException {
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTokenException() {
+		//Just needs to exist.
+	}
+	
+	@Override
+	public String toString() {
+		return "Token given is invalid.\n";
+	}
+}//End InvalidTokenException class.
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/LoginUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/LoginUser.java
@@ -1,18 +1,25 @@
 package pt.tecnico.bubbledocs.service;
 
+import pt.tecnico.bubbledocs.domain.BubbleDocs;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 
 public class LoginUser extends BubbleDocsService {
 
+	private BubbleDocs bd;
+	private String username;
+	private String password;
 	private String userToken;
 
 	public LoginUser(String username, String password) {
-		// add code here
+		this.bd = BubbleDocs.getInstance();
+		this.username = username;
+		this.password = password;
 	}
 
 	@Override
 	protected void dispatch() throws BubbleDocsException {
-		// add code here
+		bd.login(username, password);
+		userToken = bd.getTokenByUsername(username);
 	}
 
 	public final String getUserToken() {
