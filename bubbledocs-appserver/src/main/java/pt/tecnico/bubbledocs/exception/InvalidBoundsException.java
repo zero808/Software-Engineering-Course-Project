78a43b3be85931a7eb64e7a78d28fc475c65ac16
Pedro Miguel Casaleiro Dias <quitoso@gmail.com>
2015-03-28 20:01:51 +0000

    CreateSpreadSheet more test checks in success cases.

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/InvalidBoundsException.java
@@ -0,0 +1,21 @@
+package pt.tecnico.bubbledocs.exception;
+
+import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+
+public class InvalidBoundsException extends BubbleDocsException {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int row;
+	private int col;
+
+	public InvalidBoundsException(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	@Override
+	public String toString() {
+		return "Invalid bounds passed. Must be between 1 and MAX_INT. Received Row: " + row + " Col: " + col + "\n";
+	}
+}// End InvalidBoundsException class.
