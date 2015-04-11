bf57398aac4f545bde821830ed631bac90f49aec
Marco Cunha <marco.soudocunha@gmail.com>
2015-04-11 14:32:46 +0100

    closes #58

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
@@ -5,25 +5,38 @@ import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 import pt.tecnico.bubbledocs.exception.InvalidPermissionException;
 import pt.tecnico.bubbledocs.exception.LoginBubbleDocsException;
+import pt.tecnico.bubbledocs.exception.RemoteInvocationException;
+import pt.tecnico.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+import pt.tecnico.bubbledocs.service.remote.IDRemoteServices;
 
 public class DeleteUser extends BubbleDocsService {
 
 	private String userToken;
 	private String toDeleteUsername;
 
+	private IDRemoteServices idRemoteService;
+
 	public DeleteUser(String userToken, String toDeleteUsername) {
 		this.userToken = userToken;
 		this.toDeleteUsername = toDeleteUsername;
+		idRemoteService = new IDRemoteServices();
 	}
 
 	@Override
 	protected void dispatch() throws BubbleDocsException {
 		BubbleDocs bd = getBubbleDocs();
 
+		try {
+			idRemoteService.removeUser(toDeleteUsername);
+		} catch (RemoteInvocationException e) {
+			throw new UnavailableServiceException();
+		}
+
 		// root is not in session
 		if (!bd.isInSession(userToken))
-			throw new UserNotInSessionException(bd.getUsernameByToken(userToken));
+			throw new UserNotInSessionException(
+					bd.getUsernameByToken(userToken));
 
 		// user (root) does not exist
 		if (bd.getUserByUsername(bd.getUsernameByToken(userToken)) == null)
@@ -31,7 +44,8 @@ public class DeleteUser extends BubbleDocsService {
 
 		// Only root can delete users
 		if (!bd.isRoot(userToken))
-			throw new InvalidPermissionException(bd.getUsernameByToken(userToken));
+			throw new InvalidPermissionException(
+					bd.getUsernameByToken(userToken));
 
 		// user does not exist
 		if (bd.getUserByUsername(toDeleteUsername) == null)
@@ -40,11 +54,20 @@ public class DeleteUser extends BubbleDocsService {
 		User userToDelete = bd.getUserByUsername(toDeleteUsername);
 
 		userToDelete.delete();
-		
+
 		// toDeleteUser is in session,so we need to remove it.
 		String tok = bd.getTokenByUsername(toDeleteUsername);
 		if (tok != null) {
 			bd.removeUserFromSession(tok);
 		}
 	}
+
+	public IDRemoteServices getIdRemoteService() {
+		return idRemoteService;
+	}
+
+	public void setIdRemoteService(IDRemoteServices idRemoteService) {
+		this.idRemoteService = idRemoteService;
+	}
+
 }// End DeleteUser Class
