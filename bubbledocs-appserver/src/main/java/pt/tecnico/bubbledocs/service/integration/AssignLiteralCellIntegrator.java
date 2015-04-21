185e9e1c383ba4057377c7f139b5749106794637
Marco Cunha <marco.soudocunha@gmail.com>
2015-04-21 13:59:51 +0100

    This closes #102

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/integration/AssignLiteralCellIntegrator.java
@@ -0,0 +1,31 @@
+package pt.tecnico.bubbledocs.service.integration;
+
+import pt.tecnico.bubbledocs.exception.CellIsProtectedException;
+import pt.tecnico.bubbledocs.exception.InvalidPermissionException;
+import pt.tecnico.bubbledocs.exception.InvalidTokenException;
+import pt.tecnico.bubbledocs.exception.OutofBoundsException;
+import pt.tecnico.bubbledocs.exception.SpreadsheetDoesNotExistException;
+import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+import pt.tecnico.bubbledocs.service.AssignLiteralCell;
+
+public class AssignLiteralCellIntegrator extends BubbleDocsIntegrator {
+	
+	private String tokenUser;
+	private int docId;
+	private String cellId;
+	private String literal;
+	
+	public AssignLiteralCellIntegrator(String tokenUser, int docId, String cellId, String literal) {
+		this.tokenUser = tokenUser;
+		this.docId = docId;
+		this.literal = literal;
+		this.cellId = cellId;	
+	}
+
+	@Override
+	protected void dispatch() throws OutofBoundsException, CellIsProtectedException, SpreadsheetDoesNotExistException, UserNotInSessionException, InvalidTokenException, InvalidPermissionException {
+		AssignLiteralCell assignLiteralCellService = new AssignLiteralCell(tokenUser, docId, cellId, literal);
+		assignLiteralCellService.execute();
+	}
+
+}// End AssignLiteralCellIntegrator class
