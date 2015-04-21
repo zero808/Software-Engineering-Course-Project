afa732e1bea338302db7af4be18c425c91982a45
Marco Cunha <marco.soudocunha@gmail.com>
2015-04-21 14:03:42 +0100

    This closes #103

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/integration/AssignReferenceCellIntegrator.java
@@ -0,0 +1,32 @@
+package pt.tecnico.bubbledocs.service.integration;
+
+import pt.tecnico.bubbledocs.exception.CellIsProtectedException;
+import pt.tecnico.bubbledocs.exception.InvalidArgumentsException;
+import pt.tecnico.bubbledocs.exception.InvalidPermissionException;
+import pt.tecnico.bubbledocs.exception.InvalidReferenceException;
+import pt.tecnico.bubbledocs.exception.InvalidTokenException;
+import pt.tecnico.bubbledocs.exception.OutofBoundsException;
+import pt.tecnico.bubbledocs.exception.SpreadsheetDoesNotExistException;
+import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+import pt.tecnico.bubbledocs.service.AssignReferenceCell;
+
+public class AssignReferenceCellIntegrator extends BubbleDocsIntegrator {
+	
+	private String cellId;
+	private String reference;
+	private int docId;
+	private String tokenUser;
+
+	public AssignReferenceCellIntegrator(String tokenUser, int docId, String cellId, String reference) {
+		this.docId = docId;
+		this.tokenUser = tokenUser;
+		this.cellId = cellId;
+		this.reference = reference;
+	}
+
+	@Override
+	protected void dispatch() throws InvalidReferenceException, InvalidTokenException, OutofBoundsException, InvalidArgumentsException, InvalidPermissionException, CellIsProtectedException, UserNotInSessionException, SpreadsheetDoesNotExistException {
+		AssignReferenceCell assignReferenceCellService = new AssignReferenceCell(tokenUser, docId, cellId, reference);
+		assignReferenceCellService.execute();
+	}
+}// End AssignReferenceCellIntegrator class
