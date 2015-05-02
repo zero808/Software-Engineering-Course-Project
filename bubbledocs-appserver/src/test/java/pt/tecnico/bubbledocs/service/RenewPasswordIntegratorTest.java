3f2c2ad2e49b55407e6f3aabe6ee6406a72126d2
Cláudio Galante <claudio.galante@tecnico.ulisboa.pt>
2015-05-02 05:01:36 +0100

    Closes #128

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/RenewPasswordIntegratorTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/RenewPasswordIntegratorTest.java
@@ -13,6 +13,7 @@ import pt.tecnico.bubbledocs.exception.RemoteInvocationException;
 import pt.tecnico.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
 import pt.tecnico.bubbledocs.service.BubbleDocsServiceTest;
+import pt.tecnico.bubbledocs.service.integration.RenewPasswordIntegrator;
 import pt.tecnico.bubbledocs.service.remote.IDRemoteServices;
 
 public class RenewPasswordIntegratorTest extends BubbleDocsServiceTest {
@@ -35,14 +36,13 @@ public class RenewPasswordIntegratorTest extends BubbleDocsServiceTest {
 		BubbleDocs bd = getBubbleDocs();
 		String username = bd.getUsernameByToken(userToken);
 		
-		RenewPasswordService service = new RenewPasswordService(userToken);
+		RenewPasswordIntegrator service = new RenewPasswordIntegrator(userToken);
 		
 		new Expectations() {
 			{
 				idRemote.renewPassword(username);
 			}
 		};
-		service.setIDRemoteService(idRemote);
 		service.execute();
 		
 		assertNull("Password was not invalidated", bd.getUserByUsername(bd.getUsernameByToken(userToken)).getPassword());
@@ -50,21 +50,19 @@ public class RenewPasswordIntegratorTest extends BubbleDocsServiceTest {
 	
 	@Test(expected = UserNotInSessionException.class)
 	public void userNotInSession() {
-		RenewPasswordService service = new RenewPasswordService(notInSessionToken);
-		service.setIDRemoteService(idRemote);
+		RenewPasswordIntegrator service = new RenewPasswordIntegrator(notInSessionToken);
 		service.execute();
 	}
 	
 	@Test(expected = InvalidTokenException.class)
 	public void invalidToken() {
-		RenewPasswordService service = new RenewPasswordService("");
-		service.setIDRemoteService(idRemote);
+		RenewPasswordIntegrator service = new RenewPasswordIntegrator("");
 		service.execute();
 	}
 	
 	@Test(expected = UnavailableServiceException.class)
 	public void remoteInvocationFailure() {
-		RenewPasswordService service = new RenewPasswordService(userToken);
+		RenewPasswordIntegrator service = new RenewPasswordIntegrator(userToken);
 		
 		new Expectations() {
 			{
@@ -72,13 +70,12 @@ public class RenewPasswordIntegratorTest extends BubbleDocsServiceTest {
 				result = new RemoteInvocationException();
 			}
 		};
-		service.setIDRemoteService(idRemote);
 		service.execute();
 	}
 	
 	@Test(expected = LoginBubbleDocsException.class)
 	public void loginFailure() {
-		RenewPasswordService service = new RenewPasswordService(userToken);
+		RenewPasswordIntegrator service = new RenewPasswordIntegrator(userToken);
 		
 		new Expectations() {
 			{
@@ -86,7 +83,6 @@ public class RenewPasswordIntegratorTest extends BubbleDocsServiceTest {
 				result = new LoginBubbleDocsException("invalidUser");
 			}
 		};
-		service.setIDRemoteService(idRemote);
 		service.execute();
 	}
 }// End RenewPasswordIntegratorTest class
